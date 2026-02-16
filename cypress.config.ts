10656d9bf783d89be09ebdaf739411511d9cdd4e
Bharathi Babu <bharathi@MacBook-Pro-3.local>
2026-02-16 16:18:36 +0000

    Initial Cypress Cucumber Framework Setup

---
+++ cypress.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'cypress';
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+export default defineConfig({
+  
+  e2e: {
+    specPattern: "cypress/**/*.feature",
+    
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config,);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+    /*  config.env.cucumberJson = {
+        generate: true,
+        output: "cypress/cucumber-json/results.json",
+      }; */
+
+        // Enable message output
+  config.env.CUCUMBER_MESSAGES_ENABLED = true;
+  config.env.CUCUMBER_MESSAGES_OUTPUT = "cypress/reports/messages.ndjson";
+
+      return config;
+    },
+    
+  },
+  
+  
+});
